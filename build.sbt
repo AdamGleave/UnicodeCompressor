e9f1648c3254eda14b964c151432e6e4082732c5
Adam Gleave <adgleave@gmail.com>
2016-04-09 19:46:42 +0100

    Refactor CRPUTF8Compressor to take CLI options, use helper methods

--- build.sbt
+++ build.sbt
@@ -4,7 +4,9 @@ lazy val root = (project in file(".")).
     version := "0.1",
     scalaVersion := "2.11.7",
     libraryDependencies += "org.scalactic" %% "scalactic" % "2.2.6",
-    libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+    libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test",
+    libraryDependencies += "com.github.scopt" %% "scopt" % "3.4.0",
+    resolvers += Resolver.sonatypeRepo("public")
   )
   /* These were generating some errors in ScalaDoc, but not Javadoc! */
   sources in (Compile, doc) <<= sources in (Compile, doc) map { _.filterNot(_.getName startsWith "Urn") }
--- src/main/scala/uk/ac/cam/cl/arg58/mphil/compression/CRPUTF8Compressor.scala
+++ src/main/scala/uk/ac/cam/cl/arg58/mphil/compression/CRPUTF8Compressor.scala
@@ -1,69 +1,105 @@
 package uk.ac.cam.cl.arg58.mphil.compression
 
-import java.io.{FileInputStream, FileOutputStream, OutputStream}
+import java.io._
 
 import uk.ac.cam.cl.arg58.mphil.utf8._
 import uk.ac.cam.eng.ml.tcs27.compression._
 
-/**
-  * Created by adam on 07/04/16.
-  */
+case class Config(compress: Boolean = true,
+                  inFile: Option[File] = None,
+                  outFile: Option[File] = None,
+                  outputTokens: Boolean = false)
+
 object CRPUTF8Compressor {
-  def main(args: Array[String]): Unit = {
-    if (args.length < 1 || args.length > 3) {
-      System.err.println("USAGE: <COMPRESS|DECOMPRESS> [in] [out]\n")
-      System.exit(-1)
+  val parser = new scopt.OptionParser[Config]("CRPUTF8Compressor") {
+    cmd("compress") action { (_, c) =>
+      c.copy(compress = true) } text("compress") children(
+      arg[File]("<input>") optional() action { (x,c) =>
+        c.copy(inFile = Some(x)) } text("uncompressed input"),
+      arg[File]("<output>") optional() action { (x,c) =>
+        c.copy(outFile = Some(x)) } text("compressed output")
+    )
+    cmd("decompress") action { (_,c) =>
+      c.copy(compress = false) } text("decompress") children (
+      opt[Unit]("tokens") action { (_, c) =>
+        c.copy(outputTokens = true) } text("write serialised representation of Unicode tokens"),
+      arg[File]("<input>") optional() action { (x,c) =>
+        c.copy(inFile = Some(x)) } text("compressed input"),
+      arg[File]("<output>") optional() action { (x,c) =>
+        c.copy(outFile = Some(x)) } text("decompressed output")
+    )
+  }
+
+  val arith: Coder = new Arith()
+  val prior: Distribution[Token] = new UniformToken()
+  val model: AdaptiveCode[Token] = new CRPU[Token](1, 1, 0, 1, prior)
+
+  var inStream: InputStream = null
+  var outStream: OutputStream = null
+
+  def compress(): Unit = {
+    val out = new OutputStreamBitWriter(outStream)
+    arith.start_encode(out)
+
+    val utf8Decoder = new UTF8Decoder(inStream)
+    for (token <- utf8Decoder) {
+      model.encode(token, arith)
+      model.learn(token)
     }
+    model.encode(EOF(), arith)
 
-    val arith : Coder = new Arith()
-    val prior : Distribution[Token] = new UniformToken()
-    val model : AdaptiveCode[Token] = new CRPU[Token](1, 1, 0, 1, prior)
+    arith.finish_encode()
+    out.close()
+  }
 
-    val inStream =
-      if (args.length >= 2) {
-        new FileInputStream(args(1))
-      } else {
-        System.in
-      }
+  def decompress(outputTokens: Boolean): Unit = {
+    val in = new InputStreamBitReader(inStream)
 
-    val outStream =
-      if (args.length >= 3) {
-        new FileOutputStream(args(2))
-      } else {
-        System.out
-      }
+    arith.start_decode(in)
 
-    args(0) match {
-      case "COMPRESS" =>
-        val out = new OutputStreamBitWriter(outStream)
-        arith.start_encode(out)
-
-        val utf8Decoder = new UTF8Decoder(inStream)
-        for (token <- utf8Decoder) {
-          model.encode(token, arith)
-          model.learn(token)
-        }
-        model.encode(EOF(), arith)
-
-        arith.finish_encode()
-        out.close()
-      case "DECOMPRESS" =>
-        val in = new InputStreamBitReader(inStream)
-        arith.start_decode(in)
-
-        def decompress(): Unit = {
-          val token = model.decode(arith)
-          if (!token.equals(EOF())) {
-            model.learn(token)
-            val bytes = UTF8Encoder.tokenToBytes(token)
-            outStream.write(bytes)
-            decompress()
+    def decompress(): Unit = {
+      val token = model.decode(arith)
+      if (!token.equals(EOF())) {
+        model.learn(token)
+        val bytes =
+          if (outputTokens) {
+            token.toString().getBytes()
+          } else {
+            UTF8Encoder.tokenToBytes(token)
           }
-        }
+        outStream.write(bytes)
         decompress()
+      }
+    }
+    decompress()
 
-        arith.finish_decode()
-        outStream.close()
+    arith.finish_decode()
+    outStream.close()
+  }
+
+  def main(args: Array[String]): Unit = {
+    parser.parse(args, Config()) match {
+      case Some(config) =>
+        inStream = config.inFile match {
+          case Some(f) => new FileInputStream(f)
+          case None => System.in
+        }
+
+        outStream = config.outFile match {
+          case Some(f) => new FileOutputStream(f)
+          case None => System.out
+        }
+
+        if (config.compress) {
+          // compress
+          compress()
+        } else {
+          // decompress
+          decompress(config.outputTokens)
+        }
+      case None =>
+        // invalid arguments, terminate
+        System.exit(1)
     }
   }
 }

19b43e1249a50cf96948e1f2ab52c7d2ac89b818
Adam Gleave <adgleave@gmail.com>
2016-04-27 10:27:55 +0100

    benchmark suite: add support for measuring runtime

--- src/test/python/benchmark/benchmark.py
+++ src/test/python/benchmark/benchmark.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.4
 
-import asciitable, argparse, csv, errno, filecmp, functools, os, re, sys
+import asciitable, argparse, csv, errno, filecmp, functools, os, pickle, re, sys, time
 from multiprocessing import Pool
 
 from mode import CompressionMode
@@ -24,7 +24,9 @@ def check_file(original, output_prefix):
       os.unlink(decompressed_fname) # cleanup
     else:
       return "Error: decompressed file differs from original."
-  return os.path.getsize(output_prefix + ".compressed")
+  with open(output_prefix + '.done', 'r+b') as cached_file:
+    compression_time, decompression_time = pickle.load(cached_file)
+    return (os.path.getsize(output_prefix + ".compressed"), compression_time, decompression_time)
 
 def execute_compressor(compressor_name, input_fname, output_prefix):
   if verbose:
@@ -38,14 +40,20 @@ def execute_compressor(compressor_name, input_fname, output_prefix):
 
   with open(input_fname, 'r') as inFile:
     with open(compressed_fname, 'w') as outFile:
+      start_time = time.time()
       compressor(inFile, outFile, CompressionMode.compress)
+      end_time = time.time()
+      compression_time = end_time - start_time
 
   with open(compressed_fname, 'r') as inFile:
     with open(decompressed_fname, 'w') as outFile:
+      start_time = time.time()
       compressor(inFile, outFile, CompressionMode.decompress)
+      end_time = time.time()
+      decompression_time = end_time - start_time
 
-  with open(cached_fname, 'w') as cachedFile:
-    pass # just create the file to indicate we're finished
+  with open(cached_fname, 'w+b') as cachedFile:
+    pickle.dump((compression_time, decompression_time), cachedFile)
 
   return check_file(input_fname, output_prefix)
 
@@ -107,17 +115,23 @@ def human_readable_size(s):
     s /= 1024.0
   return '{0:3.1f}TB'.format(s)
 
-def table_convert(original, compressed, table_type):
-  if type(compressed) == str:
+def table_convert(original, result, table_type):
+  if type(result) == str:
     # an error message, not a number
-    return compressed
+    return result
 
   if table_type == 'bits':
+    compressed, _, _ = result
     return '{0:.4f}'.format(compressed / original * 8)
   elif table_type == 'per':
+    compressed, _, _ = result
     return '{0:.1f}%'.format(compressed / original * 100)
   elif table_type == 'size':
+    compressed, _, _ = result
     return human_readable_size(compressed)
+  elif table_type == 'time':
+    _, compression_time, decompression_time = result
+    return "{0:.3f} ; {1:.3f}".format(compression_time, decompression_time)
   else:
     assert(False)
 
@@ -134,7 +148,7 @@ if __name__ == "__main__":
   parser.add_argument('--table', dest='table_type', metavar='UNIT',
                       help='write the resutlts to stdout in an ASCII table in specified units, ' +
                       'one of bits (bits output/byte input), per (percentage output/input), ' +
-                      'size (output file size) (default: bits).')
+                      'size (output file size), time (runtime in seconds) (default: bits).')
   parser.add_argument('--invalidate', dest='invalidate', action='store_true',
                       help='remove any cached results matching the pattern.')
   parser.add_argument('--rerun', dest='rerun', action='store_true',
@@ -157,7 +171,7 @@ if __name__ == "__main__":
     csv_fname = args['csv_fname']
   if args['table_type']:
     table_type = args['table_type']
-    if table_type not in {"bits", "per", "size"}:
+    if table_type not in {"bits", "per", "size", "time"}:
       parser.error("Unrecognised table type: " + table_type)
 
   verbose = args['verbose']
@@ -192,8 +206,7 @@ if __name__ == "__main__":
     p.join()
 
   if run:
-    cols = ['Size'] + list(compressors)
-    fieldnames = ['File'] + cols
+    fieldnames = ['File', 'Size'] + compressors
 
     if csv_fname:
       with open(csv_fname, 'w') as out:
@@ -201,9 +214,12 @@ if __name__ == "__main__":
 
         writer.writeheader()
         for fname in files:
-          row = {'File': fname}
-          for compressor in cols:
-            row[compressor] = results[compressor][fname]
+          row = {'File': fname, 'Size': results['Size'][fname]}
+          for compressor in compressors:
+            size, compression_time, decompression_time = results[compressor][fname]
+            row[compressor + "_size"] = size
+            row[compressor + "_ctime"] = compression_time
+            row[compressor + "_dtime"] = decompression_time
           writer.writerow(row)
     if table_type:
       tableResults = {}

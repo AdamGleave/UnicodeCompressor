faa4840d2282d3ee84f4929f50b7a6fd410e2982
Adam Gleave <adgleave@gmail.com>
2016-05-18 13:48:50 +0100

    benchmark suite: add new training group optimal parameters

--- src/test/python/benchmark/config_benchmark.py
+++ src/test/python/benchmark/config_benchmark.py
@@ -40,6 +40,10 @@ for (algo_name, algo_config) in algos.items():
     if not is_excluded(name):
       COMPRESSORS[name] = (my_compressor, {'base': prior_name, 'algorithms': algo_config})
 
-COMPRESSORS['ppm_group_opt_uniform_byte'] = (my_compressor, {'base': 'uniform_byte', 'algorithms': ['ppm:d=6:a=-0.105494661063:b=0.47794125429']})
-COMPRESSORS['ppm_group_opt_uniform_token'] = (my_compressor, {'base': 'uniform_token', 'algorithms': ['ppm:d=6:a=-0.0588102870517:b=0.497974993229']})
-COMPRESSORS['ppm_group_opt_polya_token'] = (my_compressor, {'base': 'polya_token', 'algorithms': ['ppm:d=6:a=-0.0653849770234:b=0.49912463989']})
+COMPRESSORS['ppm_test_group_opt_uniform_byte'] = (my_compressor, {'base': 'uniform_byte', 'algorithms': ['ppm:d=6:a=-0.105494661063:b=0.47794125429']})
+COMPRESSORS['ppm_test_group_opt_uniform_token'] = (my_compressor, {'base': 'uniform_token', 'algorithms': ['ppm:d=4:a=-0.0588102870517:b=0.497974993229']})
+COMPRESSORS['ppm_test_group_opt_polya_token'] = (my_compressor, {'base': 'polya_token', 'algorithms': ['ppm:d=4:a=-0.0653849770234:b=0.49912463989']})
+
+COMPRESSORS['ppm_training_group_opt_uniform_byte'] = (my_compressor, {'base': 'uniform_byte', 'algorithms': ['ppm:d=6:a=0.0953509648641:b=0.408977501392']})
+COMPRESSORS['ppm_training_group_opt_uniform_token'] = (my_compressor, {'base': 'uniform_token', 'algorithms': ['ppm:d=5:a=0.000907185094224:b=0.513284450993']})
+COMPRESSORS['ppm_training_group_opt_polya_token'] = (my_compressor, {'base': 'polya_token', 'algorithms': ['ppm:d=5:a=0.000633029756621:b=0.513015874624']})
